a4ea283b5a9df38398f68345d81605e0597618e9
David MacMahon <davidm@astro.berkeley.edu>
2024-04-30 16:04:45 -0400

    Install iputils-ping and fping in netbootstrap.sh

--- bootstrap/netbootstrap.sh
+++ bootstrap/netbootstrap.sh
@@ -310,7 +310,7 @@ else
     #--- choose “Dialog” and "high" ---
     # === Install additional packages (if not already installed) ===
     chroot "${NETBOOT_ROOT}" apt install -y 'server-minimal^' 'openssh-server^' linux-generic nfs-common --no-install-recommends
-    chroot "${NETBOOT_ROOT}" apt install -y apt-utils less networkd-dispatcher tree
+    chroot "${NETBOOT_ROOT}" apt install -y apt-utils less networkd-dispatcher tree iputils-ping fping
     # === Remove unwanted packages ===
     chroot "${NETBOOT_ROOT}" apt remove -y --purge snapd unattended-upgrades apparmor plymouth
     chroot "${NETBOOT_ROOT}" apt remove -y --purge --autoremove landscape-common ubuntu-release-upgrader-core update-notifier-common
